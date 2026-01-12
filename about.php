2ebf44b4bb4765afdf41a4274a8f4eab066b252e
Mukaosolu <pchukwuonwe@outlook.com>
2026-01-12 02:10:55 +0100

    Initial commit: PHP app for Elastic Beanstalk deployment

---
+++ about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About - My PHP App</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>About This App</h1>
+    <p>This simple PHP app demonstrates deployment on Elastic Beanstalk.</p>
+    <a href="index.php">Home</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP App on Elastic Beanstalk</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Welcome to MK's PHP App! 🚀</h1>
+    <p>This app is hosted on <strong>AWS Elastic Beanstalk</strong>.</p>
+    <a href="about.php">About</a>
+</body>
+</html>
